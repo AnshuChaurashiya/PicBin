b274e4cf7be424d83a71fe8b0566029517dddfa6
AnshuChaurashiya <chaurashiya0016@gmail.com>
2025-06-15 02:38:25 -0700

    first commit

--- Server/routes/imageRoute.js
+++ Server/routes/imageRoute.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const { uploadImage, getMyImages, detletImages } = require('../controller/ImageController');
-const upload = require('../middleware/uploadMiddleware');
-const authUser = require('../middleware/Auth');
+const UserController = require('../controller/User_Controller');
+
+// Upload middleware configuration
+const storage = multer.diskStorage({
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage });
  
-router.post('/upload', authUser, upload.single('file'), uploadImage);
-router.get('/my-uploads', authUser, getMyImages);
-router.delete('/delete/:imageId', authUser, detletImages);
+router.post('/upload', UserController.AuthUser, upload.single('file'), uploadImage);
+router.get('/my-uploads', UserController.AuthUser, getMyImages);
+router.delete('/delete/:imageId', UserController.AuthUser, detletImages);
 
 module.exports = router;
